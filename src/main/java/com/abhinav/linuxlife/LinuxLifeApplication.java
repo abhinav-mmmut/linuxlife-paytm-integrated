246af02d51c4012b76368e98a72021fff8bd4eb1
abhinav <abhinav.ec.mmmut@gmail.com>
2021-02-11 19:15:54 +0530

    paytm payment integration done

---
+++ src/main/java/com/abhinav/linuxlife/LinuxLifeApplication.java
@@ -0,0 +1,13 @@
+package com.abhinav.linuxlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxLifeApplication.class, args);
+	}
+
+}
